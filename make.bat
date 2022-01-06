59a55c2c1f0ac4b67934a1695547c84f48199117
ivansoft <32499694+ivansoft@users.noreply.github.com>
2022-01-06 17:22:48 +0300

    [cli] use Makefile on Windows

--- make.bat
+++ make.bat
@@ -2,87 +2,40 @@
 
 pushd %~dp0
 
-if "%~1" == "" goto empty
+setlocal
 
-CALL :check_python
+if "%~1"=="help" goto :help
+if not "%~1"=="" set "PATH=%~1;%PATH%"
+
+if not defined MSYS_HOME set MSYS_HOME=c:\msys64
+set "MSYS_USR_BIN=%MSYS_HOME%\usr\bin"
+set "PATH=%MSYS_USR_BIN%;%PATH%"
+
+CALL :check_make
 if errorlevel 1 goto :end
 
-2>NUL CALL :COMMAND_%~1
-if errorlevel 1 goto :unknown
-exit /b
+make.exe %*
 
-::
-:: Aka https://docs.npmjs.com/cli/v8/using-npm/scripts
-::
-:COMMAND_install
-	pip install -U pip setuptools
-	pip install -Ur requirements-dev.txt
-	goto :end
-:COMMAND_version
-	python -V
-	pip -V
-	echo VIRTUAL_ENV: %VIRTUAL_ENV%
-	goto :end
-:COMMAND_uninstall
-	pip list --exclude pip --exclude setuptools --format freeze > __temp.txt
-	type __temp.txt
-	pip uninstall -yr __temp.txt
-	del /f /q __temp.txt
-	goto :end
-
-
-:unknown
-echo make: *** No rule to make target '%~1'.  Stop.
-goto :help
-
-:empty
-echo make: *** No targets specified.  Stop.
-goto :help
+goto :end
 
 :help
-echo Available commands:
-echo  install - install all requirements
-echo  version - Python / pip version numbers
-echo  uninstall - uninstall all requirements
+echo Usage: make [msys_home_folder]
+echo msys_home_folder - Path to your ^<msys2^> installation folder.
+echo                    This path will be added to PATH environment variable.
 
 :end
+endlocal
 popd
 exit /b
 
-:check_python
-
-python -V >NUL 2>NUL
-if errorlevel 9009 (
-	echo.
-	echo The 'python' command was not found.
-	echo.
-	echo If you don't have Python installed, grab it from
-	echo https://www.python.org/downloads/
-	exit /b /1
-)
-
-pip -V >NUL 2>NUL
-if errorlevel 9009 (
-	echo.
-	echo The 'pip' command was not found.
-	echo.
-	echo If your Python environment does not have pip installed, there are 2
-	echo mechanisms to install pip supported directly by pip’s maintainers:
-	echo * ensurepip ^(https://docs.python.org/3/library/ensurepip.html^)
-	echo * get-pip.py ^(https://bootstrap.pypa.io/get-pip.py^)
-	exit /b /1
-)
-
-python -c "import os; print(os.environ['VIRTUAL_ENV'])" >NUL 2>NUL
+:check_make
+>NUL where make
 if errorlevel 1 (
 	echo.
-	echo Python is running outside virtual environment.
-	echo.
-	echo Use venv to isolate libraries and scripts from those installed in other
-	echo environments and ^(by default^) from a "system" Python environment.
-	echo.
-	echo For more information please visit:
-	echo https://docs.python.org/3/library/venv.html
+	echo 'make.exe' is not recognized as an internal or external command,
+	echo operable program or batch file.
+ 	echo Please set up MSYS_HOME environment variable to your MSYS home folder
+ 	echo or pass path via command line parameter - see `make help'.
+	echo https://www.msys2.org/#installation
 	exit /b /1
 )
-
